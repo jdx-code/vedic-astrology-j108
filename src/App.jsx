72c0bf680fba06f704265340c0197dc5f878a412
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-07-31 12:03:13 +0530

    About section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
 import './App.css'
+import about from './about.js'
+import salutations from './salutations.js'
 import Hero from './components/Hero'
 import About from './components/About'
 import Cards from './components/Cards'
 
+
 function App() {
   
+  const cards = salutations.map(item => {
+    return (
+      <Cards 
+        img={item.img}
+        name={item.name}
+        url={item.url}
+      />
+    )
+  })
+
   return (
     <>      
       <Navbar />      
       <Hero />
-      <About />
-      <Cards />
+      <About content={about[0].content} url={about[0].url}/>
+      <section className='flex flex-wrap justify-center text-sm mt-4 mb-20'>        
+        {cards}
+      </section>      
+      <About content={about[1].content} url={about[1].url}/>
     </>
   )
 }
---
+++ src/about.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        content: "jyotish108 is an online organization devoted to clearing gross misconceptions prevalent in Vedic astrology and making genuine Jyotish services available to the masses at cheaper rates. At a time, when reputed online astrologers charge a very large sum for a basic astrological consultation, we at jyotish108 are committed to providing similar services at extremely low costs including remedies that can be easily done without spending a dime! It is our guarantee that your life problems can be solved without going for costly gemstones and pendants and as such it is our resolve NEVER to make our clients go for costly and practically useless gem therapy. Occult remedies are also available as per requirement of the client. Do consult once to experience the difference!.", 
+        url:"/001.mp4"
+    },
+    {
+        content:"My name is Devarshi Dutta and I am a forever student of the Western Occult and Vedic astrology. My mission is to dispel the myths that have been plaguing Vedic astrology for quite some time now and to give astrological guidance to the ones seeking my help without going into remedies like costly Pujas and gemstones. I have been practicing astrology since 2011 and although I do not claim to belong to any particular tradition (parampara), I consider myself fortunate to be guided by renowned astrologers like Pt. Katti Narahari, Shri Michael Reed, Shri Visti Larsen, Shri Freedom Cole, E.K. Dhilip Kumar and Shri Manish Pandit. I currently reside in my home city of Guwahati, Assam, India and see clients and do horoscope readings on a daily basis.",
+        url:"/devarshi.jpeg"
+    }
+]
